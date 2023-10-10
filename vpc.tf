3879938b611a9139eaf9d8606e95bc53967dd427
Yuvraj Sakhare <147066313+yuvrajsakhare@users.noreply.github.com>
2023-10-10 11:05:09 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,9 @@
+resource "aws_vpc" "provisionerVPC"{
+    cidr_block = var.vpc_cidr
+    enable_dns_hostnames=true
+    enable_dns_support = true
 
+    tags = {
+        Name = "dev-terraform-vpc"
+    }
+}
